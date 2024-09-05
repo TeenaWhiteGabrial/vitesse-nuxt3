e0664c34d2bcf78aa94e09d00eeaae9150e4e801
kunlun zhang <zhangkunlun01@inspur.com>
2024-09-05 18:09:09 +0800

    feat: v1.0

---
+++ pages/chatLLM/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wh-full">
+    <button class="btn">
+      111
+    </button>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,16 @@
-<template>
-  <div class="contain">
-    <Banner></Banner>
-    <Points></Points>
-  </div>
-</template>
 <script setup lang="ts">
   // import type { ResponceResult } from '#/fetch.d.ts'
   // const configData: ResponceResult = await $fetch('gateway/portal/open/configs', {
   //   baseURL: 'http://localhost:8080/'
   // })
   // const bannerJson = configData.data?.bannerJson
-
 </script>
-<style ></style>
 
+<template>
+  <div class="contain">
+    <Banner />
+    <Points />
+  </div>
+</template>
 
+<style></style>
